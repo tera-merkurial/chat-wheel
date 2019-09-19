a2b239828ba86e927c47c5d817e71d31ff5624ab
Merkurial <55410235+tera-merkurial@users.noreply.github.com>
2019-09-19 11:29:13 -0300

    C# mods when?

--- libs/game-state.party.js
+++ libs/game-state.party.js
@@ -12,21 +12,21 @@ class Party {
 
     installHooks() {
         this.mod.hook('S_PARTY_MEMBER_LIST', 7, event => {
-            this.inRaid = event.raid;
+            this.m_inRaid = event.raid;
             var sameServer = event.leaderServerId == this.mod.game.me.serverId;
             var samePlayerId = event.leaderPlayerId == this.mod.game.me.playerId;
-            this.isLeader = sameServer && samePlayerId;
+            this.m_isLeader = sameServer && samePlayerId;
         })
 
         this.mod.hook('S_CHANGE_PARTY_MANAGER', 2, event => {
             var sameServer = event.serverId == this.mod.game.me.serverId;
             var samePlayerId = event.playerId == this.mod.game.me.playerId;
-            this.isLeader = sameServer && samePlayerId;
+            this.m_isLeader = sameServer && samePlayerId;
         })
 
         this.mod.hook('S_LEAVE_PARTY', 1, event => {
-            this.inRaid = false;
-            this.isLeader = false;
+            this.m_inRaid = false;
+            this.m_isLeader = false;
         })
 
         this.mod.hook('S_RETURN_TO_LOBBY', 'raw', (event) => {
